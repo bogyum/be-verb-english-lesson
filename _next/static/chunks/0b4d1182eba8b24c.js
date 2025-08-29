c0d7561a21eba215fe4a38a5c85599531441d141
김보겸 <bogyum@gimbogyeom-ui-MacBookPro.local>
2025-08-29 20:46:51 +0900

    Update gh-pages with CSS animations

---
+++ _next/static/chunks/0b4d1182eba8b24c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/0cbcb654933e15ed.js",
+  "static/chunks/turbopack-caaf5b70f0e14895.js"
+])
---
+++ _next/static/chunks/df8e875986acdf4b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/0cbcb654933e15ed.js",
+  "static/chunks/turbopack-3bb131376cd194e5.js"
+])
